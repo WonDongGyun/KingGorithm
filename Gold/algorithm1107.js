74420ac73bd898bbbc0de7ee5e49651be6db3638
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-12-17 01:00:45 +0900

    Comment: 1107번 주석 정리
    1. 1107번 주석 정리

--- Gold/algorithm1107.js
+++ Gold/algorithm1107.js
@@ -76,15 +76,18 @@ ps.main(async () => {
   const broken = parseInt(await ps.readLine());
   const brokenArr = new Array(10).fill(false);
 
-  (await ps.readLine()).split(" ").forEach((element) => {
-    brokenArr[element] = true;
-  });
+  // 리모콘에 부서진 버튼이 없는 경우도 있다.
+  if (broken > 0) {
+    (await ps.readLine()).split(" ").forEach((element) => {
+      brokenArr[element] = true;
+    });
+  }
 
   // 처음 시작 채널은 100번 채널입니다.
   let result = Math.abs(N - 100);
 
-  const remote = (button) => {
-    if (button === 0) {
+  const remote = (channel) => {
+    if (channel === 0) {
       if (brokenArr[0]) {
         return 0;
       } else {
@@ -92,24 +95,31 @@ ps.main(async () => {
       }
     }
 
-    let len = 0;
-    while (button > 0) {
-      // 리모컨 버튼이 고장났다면 눌러도 소용이 없으니 0을 반환합니다.
-      if (brokenArr[button % 10]) return 0;
+    let click = 0;
 
-      // 리모컨 버튼이 고장나지 않았다면 횟수를 셉니다.
-      button = parseInt(button / 10);
-      len += 1;
+    // 해당 채널을 10으로 나누어가며 각 자리수마다 리모컨 버튼의 고장 유무를 검사합니다.
+    while (channel > 0) {
+      // 해당 자리수의 리모컨 버튼이 고장났다면 눌러도 소용이 없으니 0을 반환합니다.
+      if (brokenArr[channel % 10]) return 0;
+
+      // 리모컨 버튼이 고장나지 않았다면 횟수를 셉니다. 해당 버튼을 눌렀기 때문에 다음 자리수로 넘어가면 됩니다.
+      // 리모컨의 고장나지 않은 버튼을 눌러서 이동하는 것이므로 click 수는 하나씩만 증가시킵니다.
+      channel = parseInt(channel / 10);
+      click++;
     }
 
-    return len;
+    return click;
   };
+
+  /* 최대 500000 채널까지 입력할 수 있지만, 고장나지않은 버튼을 사용해서 100채널에서 시작해서
+   상위의 채널로 이동 후 시작할 수 있기 때문에 0 ~1000000번 채널까지 모든 경우의 수를 검토합니다. */
 
   for (let i = 0; i <= 1000000; i++) {
     let len = remote(i);
 
     if (len > 0) {
-      //
+      // press는 + 혹은 - 버튼을 사용해서 1씩 이동한 경우의 수가 저장됩니다.
+      // press + len ==> 고장나지 않은 리모컨 버튼을 사용해서 가장 가까운 채널로 이동 + (+, -) 버튼을 사용해 조정한 경우
       const press = Math.abs(i - N);
       if (result > press + len) {
         result = press + len;
