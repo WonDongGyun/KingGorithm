fa6becc713e2bc83bd11bd715e91820e9c27bdf9
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-12-20 23:57:37 +0900

    File: 실버 2문제 해결
    1. 실버 2문제 해결
    - 11726(2xn 타일링) [다이나믹 프로그래밍]
    - 11727(2xn 타일링 2) [다이나믹 프로그래밍]

---
+++ Silver/algorithm11727.js
@@ -0,0 +1,92 @@
+// 원동균 / 11727 / 2xn 타일링 2
+"use strict";
+
+const { memory } = require("console");
+
+const ps = (function (process) {
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let lines = [];
+  let cursor = 0;
+
+  rl.on("line", function (line) {
+    lines.push(line);
+  });
+
+  return {
+    main(f) {
+      f()
+        .catch((err) => {
+          console.error(err);
+          process.exit(1);
+        })
+        .finally(() => {
+          rl.close();
+        });
+    },
+    use(name, f) {
+      this[name] = f;
+    },
+    readLine: async function readLine() {
+      return new Promise((resolve) => {
+        if (cursor < lines.length) {
+          resolve(lines[cursor++]);
+        } else {
+          setTimeout(() =>
+            readLine().then((line) => {
+              resolve(line);
+            })
+          );
+        }
+      });
+    },
+    async readArrayLine() {
+      const line = await this.readLine();
+      return line.split(/\s/).map((t) => parseInt(t));
+    },
+    write(data) {
+      process.stdout.write(data + "");
+    },
+    writeLine(data) {
+      process.stdout.write((data === undefined ? "" : data) + "\n");
+    },
+    range(start, end, step = 1) {
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      }
+      return {
+        [Symbol.iterator]: function* () {
+          for (let i = start; i < end; i += step) {
+            yield i;
+          }
+        },
+      };
+    },
+  };
+})(process);
+
+ps.main(async () => {
+  const n = parseInt(await ps.readLine());
+  const dp = Array(1001).fill(0);
+  dp[1] = 1;
+  dp[2] = 3;
+
+  const fib = (num) => {
+    if (num < 3) {
+      return dp[num] % 10007;
+    }
+    for (let i = 3; i <= num; i++) {
+      dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+    }
+
+    return dp[num];
+  };
+
+  console.log(fib(n));
+});
