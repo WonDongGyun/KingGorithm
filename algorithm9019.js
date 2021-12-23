fe37e763ad0d45070fdf8d5a83f8cb29bf2b43fd
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-12-23 23:57:52 +0900

    File: 골드 1문제 보류
    1. 골드 1문제 보류
    - 9019(DSLR) [시간 초과]

---
+++ algorithm9019.js
@@ -0,0 +1,128 @@
+// 원동균 / 9019 / DSLR
+"use strict";
+
+const { memory } = require("console");
+
+const ps = (function (process) {
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let lines = [];
+  let cursor = 0;
+
+  rl.on("line", function (line) {
+    lines.push(line);
+  });
+
+  return {
+    main(f) {
+      f()
+        .catch((err) => {
+          console.error(err);
+          process.exit(1);
+        })
+        .finally(() => {
+          rl.close();
+        });
+    },
+    use(name, f) {
+      this[name] = f;
+    },
+    readLine: async function readLine() {
+      return new Promise((resolve) => {
+        if (cursor < lines.length) {
+          resolve(lines[cursor++]);
+        } else {
+          setTimeout(() =>
+            readLine().then((line) => {
+              resolve(line);
+            })
+          );
+        }
+      });
+    },
+    async readArrayLine() {
+      const line = await this.readLine();
+      return line.split(/\s/).map((t) => parseInt(t));
+    },
+    write(data) {
+      process.stdout.write(data + "");
+    },
+    writeLine(data) {
+      process.stdout.write((data === undefined ? "" : data) + "\n");
+    },
+    range(start, end, step = 1) {
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      }
+      return {
+        [Symbol.iterator]: function* () {
+          for (let i = start; i < end; i += step) {
+            yield i;
+          }
+        },
+      };
+    },
+  };
+})(process);
+
+ps.main(async () => {
+  const T = parseInt(await ps.readLine());
+  let result = "";
+
+  const bps = (A, B) => {
+    const queue = [];
+    const visited = Array(10000);
+    for (var i = 0; i < 10000; ++i) visited[i] = false;
+
+    queue.unshift([A, ""]);
+
+    while (queue.length > 0) {
+      const [before, command] = queue.push();
+      visited[before] = true;
+
+      if (before === B) return command;
+
+      // 각 if 문마다 새로운 값들이 queue로 들어갑니다.
+
+      let after = 0;
+      after = (2 * before) % 10000;
+      if (!visited[after]) {
+        queue.unshift([after, command + "D"]);
+        visited[after] = true;
+      }
+
+      after = before - 1 === 0 ? 9999 : before - 1;
+      if (!visited[after]) {
+        queue.unshift([after, command + "S"]);
+        visited[after] = true;
+      }
+
+      after = (before % 1000) * 10 + parseInt(before / 1000);
+      if (!visited[after]) {
+        queue.unshift([after, command + "L"]);
+        visited[after] = true;
+      }
+
+      after = (before % 10) * 1000 + parseInt(before / 10);
+      if (!visited[after]) {
+        queue.unshift([after, command + "R"]);
+        visited[after] = true;
+      }
+    }
+  };
+
+  for (let i = 0; i < T; i++) {
+    const [A, B] = (await ps.readLine())
+      .split(" ")
+      .map((element) => parseInt(element));
+
+    result += `${bps(A, B)}\n`;
+  }
+  console.log(result);
+});
