601e91be16e894f49384199e92ec3a2c4fc95434
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-12-26 23:30:41 +0900

    File: 브론즈 2문제 해결
    1. 브론즈 2문제 해결
    - 10757(큰 수 A+B) [큰 수 연산]
    - 2750(수 정렬하기) [정렬]

---
+++ algorithm2750.js
@@ -0,0 +1,86 @@
+// 원동균 / 2750 / 수 정렬하기
+"use strict";
+
+const { memory } = require("console");
+
+const ps = (function (process) {
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let lines = [];
+  let cursor = 0;
+
+  rl.on("line", function (line) {
+    lines.push(line);
+  });
+
+  return {
+    main(f) {
+      f()
+        .catch((err) => {
+          console.error(err);
+          process.exit(1);
+        })
+        .finally(() => {
+          rl.close();
+        });
+    },
+    use(name, f) {
+      this[name] = f;
+    },
+    readLine: async function readLine() {
+      return new Promise((resolve) => {
+        if (cursor < lines.length) {
+          resolve(lines[cursor++]);
+        } else {
+          setTimeout(() =>
+            readLine().then((line) => {
+              resolve(line);
+            })
+          );
+        }
+      });
+    },
+    async readheapayLine() {
+      const line = await readLine();
+      return line.split(/\s/).map((t) => parseInt(t));
+    },
+    write(data) {
+      process.stdout.write(data + "");
+    },
+    writeLine(data) {
+      process.stdout.write((data === undefined ? "" : data) + "\n");
+    },
+    range(start, end, step = 1) {
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      }
+      return {
+        [Symbol.iterator]: function* () {
+          for (let i = start; i < end; i += step) {
+            yield i;
+          }
+        },
+      };
+    },
+  };
+})(process);
+
+ps.main(async () => {
+  const N = parseInt(await ps.readLine());
+  const arr = [];
+  for (let i = 0; i < N; i++) {
+    arr.push(parseInt(await ps.readLine()));
+  }
+
+  arr.sort((a, b) => a - b);
+
+  for (const index of arr) {
+    console.log(index);
+  }
+});
