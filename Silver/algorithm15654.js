1a12de232fa02da556134438e4bdd964cb0b0b84
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2022-01-01 23:42:52 +0900

    File: 실버 1문제 해결
    1. 실버 1문제 해결
    - 15654(N과 M (5)) [백트래킹, 순열]

---
+++ Silver/algorithm15654.js
@@ -0,0 +1,104 @@
+// 원동균 / 15654 / N과 M (5)
+"use strict";
+
+const { memory } = require("console");
+
+const ps = (function (process) {
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let lines = [];
+  let cursor = 0;
+
+  rl.on("line", function (line) {
+    lines.push(line);
+  });
+
+  return {
+    main(f) {
+      f()
+        .catch((err) => {
+          console.error(err);
+          process.exit(1);
+        })
+        .finally(() => {
+          rl.close();
+        });
+    },
+    use(name, f) {
+      this[name] = f;
+    },
+    readLine: async function readLine() {
+      return new Promise((resolve) => {
+        if (cursor < lines.length) {
+          resolve(lines[cursor++]);
+        } else {
+          setTimeout(() =>
+            readLine().then((line) => {
+              resolve(line);
+            })
+          );
+        }
+      });
+    },
+    async readheapayLine() {
+      const line = await readLine();
+      return line.split(/\s/).map((t) => parseInt(t));
+    },
+    write(data) {
+      process.stdout.write(data + "");
+    },
+    writeLine(data) {
+      process.stdout.write((data === undefined ? "" : data) + "\n");
+    },
+    range(start, end, step = 1) {
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      }
+      return {
+        [Symbol.iterator]: function* () {
+          for (let i = start; i < end; i += step) {
+            yield i;
+          }
+        },
+      };
+    },
+  };
+})(process);
+
+ps.main(async () => {
+  const [N, M] = (await ps.readLine())
+    .split(" ")
+    .map((element) => parseInt(element));
+
+  const numArr = (await ps.readLine())
+    .split(" ")
+    .map((element) => parseInt(element));
+
+  numArr.sort((a, b) => a - b);
+
+  const getPermutations = function (arr, selectNumber) {
+    const results = [];
+    if (selectNumber === 1) return arr.map((el) => [el]);
+
+    arr.forEach((fixed, index, origin) => {
+      const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+      const permutations = getPermutations(rest, selectNumber - 1);
+      const attached = permutations.map((el) => [fixed, ...el]);
+      results.push(...attached);
+    });
+
+    return results;
+  };
+
+  const result = getPermutations(numArr, M);
+
+  for (let i of result) {
+    console.log(i.join(" "));
+  }
+});
