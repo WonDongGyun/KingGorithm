0311a0b629c0eaaf36a45acec3269ef2e0b4a47b
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2022-01-11 23:50:42 +0900

    File: 브론즈 3문제 해결
    1. 브론즈 3문제 해결
    - 2845(파티가 끝나고 난 뒤) [수학, 구현]
    - 2914(저작권) [수학, 구현]
    - 3046(R2) [수학, 구현]

---
+++ Bronze/algorithm2845.js
@@ -0,0 +1,91 @@
+// 원동균 / 2845 / 파티가 끝나고 난 뒤
+"use strict";
+
+const { memory } = require("console");
+
+const ps = (function (process) {
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let lines = [];
+  let cursor = 0;
+
+  rl.on("line", function (line) {
+    lines.push(line);
+  });
+
+  return {
+    main(f) {
+      f()
+        .catch((err) => {
+          console.error(err);
+          process.exit(1);
+        })
+        .finally(() => {
+          rl.close();
+        });
+    },
+    use(name, f) {
+      this[name] = f;
+    },
+    readLine: async function readLine() {
+      return new Promise((resolve) => {
+        if (cursor < lines.length) {
+          resolve(lines[cursor++]);
+        } else {
+          setTimeout(() =>
+            readLine().then((line) => {
+              resolve(line);
+            })
+          );
+        }
+      });
+    },
+    async readheapayLine() {
+      const line = await readLine();
+      return line.split(/\s/).map((t) => parseInt(t));
+    },
+    write(data) {
+      process.stdout.write(data + "");
+    },
+    writeLine(data) {
+      process.stdout.write((data === undefined ? "" : data) + "\n");
+    },
+    range(start, end, step = 1) {
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      }
+      return {
+        [Symbol.iterator]: function* () {
+          for (let i = start; i < end; i += step) {
+            yield i;
+          }
+        },
+      };
+    },
+  };
+})(process);
+
+ps.main(async () => {
+  const [L, P] = (await ps.readLine())
+    .split(" ")
+    .map((element) => parseInt(element));
+
+  const member = L * P;
+  let result = [];
+
+  const news = (await ps.readLine())
+    .split(" ")
+    .map((element) => parseInt(element));
+
+  for (let index of news) {
+    result.push(index - member);
+  }
+
+  console.log(result.join(" "));
+});
