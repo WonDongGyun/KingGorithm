b5bbb57819915eaec059937b19251dc412344e81
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2022-01-25 00:15:01 +0900

    File: 브론즈 1문제 해결
    1. 브론즈 1문제 해결
    - 10170(NFC West vs North) [구현]

---
+++ Bronze/algorithm10170.js
@@ -0,0 +1,88 @@
+// 원동균 / 10170 / NFC West vs North
+"use strict";
+
+const { memory } = require("console");
+
+const ps = (function (process) {
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let lines = [];
+  let cursor = 0;
+
+  rl.on("line", function (line) {
+    lines.push(line);
+  });
+
+  return {
+    main(f) {
+      f()
+        .catch((err) => {
+          console.error(err);
+          process.exit(1);
+        })
+        .finally(() => {
+          rl.close();
+        });
+    },
+    use(name, f) {
+      this[name] = f;
+    },
+    readLine: async function readLine() {
+      return new Promise((resolve) => {
+        if (cursor < lines.length) {
+          resolve(lines[cursor++]);
+        } else {
+          setTimeout(() =>
+            readLine().then((line) => {
+              resolve(line);
+            })
+          );
+        }
+      });
+    },
+    async readheapayLine() {
+      const line = await readLine();
+      return line.split(/\s/).map((t) => parseInt(t));
+    },
+    write(data) {
+      process.stdout.write(data + "");
+    },
+    writeLine(data) {
+      process.stdout.write((data === undefined ? "" : data) + "\n");
+    },
+    range(start, end, step = 1) {
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      }
+      return {
+        [Symbol.iterator]: function* () {
+          for (let i = start; i < end; i += step) {
+            yield i;
+          }
+        },
+      };
+    },
+  };
+})(process);
+
+ps.main(async () => {
+  console.log("NFC West       W   L  T");
+  console.log("-----------------------");
+  console.log("Seattle        13  3  0");
+  console.log("San Francisco  12  4  0");
+  console.log("Arizona        10  6  0");
+  console.log("St. Louis      7   9  0");
+  console.log("");
+  console.log("NFC North      W   L  T");
+  console.log("-----------------------");
+  console.log("Green Bay      8   7  1");
+  console.log("Chicago        8   8  0");
+  console.log("Detroit        7   9  0");
+  console.log("Minnesota      5  10  1");
+});
