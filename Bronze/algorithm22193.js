c9051b8a6545e21cc877e65fcbcfc4698fd37e24
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2022-02-12 01:55:09 +0900

    File: 브론즈 1문제 해결
    1. 브론즈 1문제 해결
    - 22193(Multiply) [사칙연산, 큰 수 연산]

--- Bronze/algorithm22193.js
+++ Bronze/algorithm22193.js
@@ -1,4 +1,4 @@
-// 원동균 / 1092 / 배
+// 원동균 / 22193 / Multiply
 "use strict";
 
 const { memory } = require("console");
@@ -72,33 +72,9 @@ const ps = (function (process) {
 })(process);
 
 ps.main(async () => {
-  const N = parseInt(await ps.readLine());
-  const crane = (await ps.readLine()).split(" ").map((el) => parseInt(el));
-  const M = parseInt(await ps.readLine());
-  const box = (await ps.readLine()).split(" ").map((el) => parseInt(el));
-  let result = 0;
+  const [N, M] = (await ps.readLine()).split(" ").map((el) => parseInt(el));
+  const A = BigInt(await ps.readLine());
+  const B = BigInt(await ps.readLine());
 
-  crane.sort((a, b) => b - a);
-  box.sort((a, b) => b - a);
-
-  if (crane[0] < box[0]) {
-    console.log(-1);
-  } else {
-    while (box.length != 0) {
-      // 하나의 크래인은 하나의 상자만 옮길 수 있습니다.
-      // 따라서 하나 옮기면 다음 크레인을 사용해서 옮겨야 합니다.
-      for (let i = 0; i < N; i++) {
-        for (let j = 0; j < M; j++) {
-          if (crane[i] >= box[j]) {
-            box.splice(j, 1);
-            break;
-          }
-        }
-      }
-
-      // N개의 크래인을 사용해서 box를 옮긴후 상자가 빌때까지 다시 while 루프를 수행합니다.
-      result++;
-    }
-    console.log(result);
-  }
+  console.log(String(A * B));
 });
